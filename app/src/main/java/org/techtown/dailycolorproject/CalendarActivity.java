944ca852275705cd958dfa811c700af7eaa598bd
jiyujung <s2019w39@e-mirim.hs.kr>
2020-11-19 00:30:13 +0900

    yujung's commit - Add onCalendar, settings bottom menu intent

--- app/src/main/java/org/techtown/dailycolorproject/CalendarActivity.java
+++ app/src/main/java/org/techtown/dailycolorproject/CalendarActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CalendarActivity extends AppCompatActivity {
+    ImageButton onCalendar, pixeldiary, settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,38 @@ public class CalendarActivity extends AppCompatActivity {
                 overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
             }
         });
+
+        onCalendar = findViewById(R.id.oncalendar_click1);
+        onCalendar.setOnClickListener(btnListener);
+
+        pixeldiary = findViewById(R.id.pixeldiary_unclick1);
+        pixeldiary.setOnClickListener(btnListener);
+
+        settings = findViewById(R.id.settings_unclick1);
+        settings.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        Intent intent;
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.oncalendar_click1:
+                    intent = new Intent(getApplicationContext(), CalendarActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                    break;
+                case R.id.pixeldiary_unclick1:
+                    intent = new Intent(getApplicationContext(), PixeldiaryActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                    break;
+                case R.id.settings_unclick1:
+                    intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                    break;
+            }
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+        }
+    };
 }
--- app/src/main/java/org/techtown/dailycolorproject/SettingsActivity.java
+++ app/src/main/java/org/techtown/dailycolorproject/SettingsActivity.java
@@ -2,13 +2,50 @@ package org.techtown.dailycolorproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SettingsActivity extends AppCompatActivity {
+    ImageButton onCalendar, pixeldiary, settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        onCalendar = findViewById(R.id.oncalendar_click3);
+        onCalendar.setOnClickListener(btnListener);
+
+        pixeldiary = findViewById(R.id.pixeldiary_unclick3);
+        pixeldiary.setOnClickListener(btnListener);
+
+        settings = findViewById(R.id.settings_unclick3);
+        settings.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        Intent intent;
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.oncalendar_click3:
+                    intent = new Intent(getApplicationContext(), CalendarActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                    break;
+                case R.id.pixeldiary_unclick3:
+                    intent = new Intent(getApplicationContext(), PixeldiaryActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                    break;
+                case R.id.settings_unclick3:
+                    intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                    break;
+            }
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+        }
+    };
 }
